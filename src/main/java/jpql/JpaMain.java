6a535684820aa9873571550497247df95abec6f8
csupreme19 <csupreme19@gmail.com>
2022-09-21 22:39:55 +0900

    SELECT 프로젝션

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -14,6 +15,39 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            // Query
+            List resultList = em.createQuery("select distinct m.username, m.age from Member m")
+                    .getResultList();
+            Object[] result = (Object[]) resultList.get(0);
+            for(Object o : result) {
+                System.out.println(o);
+            }
+
+            // TypedQuery
+            List<Object[]> resultList2 = em.createQuery("select distinct m.username, m.age from Member m")
+                    .getResultList();
+            for(Object[] result2 : resultList2) {
+                for(Object o : result2) {
+                    System.out.println(o);
+                }
+            }
+
+            // new로 조회
+            List<MemberDTO> resultList3 = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+            for (MemberDTO memberDTO : resultList3) {
+                System.out.println(memberDTO.getUsername());
+                System.out.println(memberDTO.getAge());
+            }
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpql/MemberDTO.java
+++ src/main/java/jpql/MemberDTO.java
@@ -1,24 +1,24 @@
 package jpql;
 
-import javax.persistence.*;
 import java.util.List;
 
-@Entity
-public class Member {
+public class MemberDTO {
 
-    @Id
-    @GeneratedValue
     private Long id;
 
     private String username;
     private int age;
 
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
     private Team team;
-
-    @OneToMany(mappedBy = "member")
     private List<Order> orders;
+
+    public MemberDTO() {
+    }
+
+    public MemberDTO(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
 
     public Long getId() {
         return id;
